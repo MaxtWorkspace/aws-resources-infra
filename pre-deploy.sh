592757b14867bad50755bec1f6fe0e0891781be8
XIAOTMA <49177628+maxt0214@users.noreply.github.com>
2023-03-05 08:35:35 -0700

    fix: fixed zip file to exclude directory structure

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,8 +1,7 @@
 for f in ./src/*; do
   if [ -d "$f" ]; then
-    echo $f;
-    go test -v $f
+    echo Bundling: $f
     go build -o $f/main $f
-    zip $f.zip $f/main
+    zip -j $f.zip $f/main
   fi
 done;
